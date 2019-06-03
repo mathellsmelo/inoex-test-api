d50aacfa9baf1438d6d178d123a182a59f81584a
matheusmelodev <matt.melo98@gmail.com>
2019-06-03 20:39:22 -0300

    mudanla no valor do amount

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const axios = require('axios')
+const axios = require('axios');
 const api = axios.create({
     baseURL: 'https://api.blockcypher.com/v1/bcy/test'
 });
@@ -7,15 +7,16 @@ main();
 
 function main () {
     const address = 'C3qXd3YYRBrZZod5D9GLGhpDNnVfAwpbe2';
-    const amount = '0.1';
-    const token = 'f559581fbebb412b9379cd5752bcae49'
-    api.post(`/faucet?token=?${token}`, {
-        address,
+    const amount = 1;
+    const token = 'f559581fbebb412b9379cd5752bcae49';
+    api.post(`/faucet?token=${token}`, {
+        address, 
         amount
     }).then(res => {
-        console.log(res);
+        console.log(res.data);
     }).catch(err => {
         console.log(err.response.data.error);
     });
 }
+
 
