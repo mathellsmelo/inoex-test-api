5c5a4d41e0fbe3407de621f993fc96cc90b6046c
matheusmelodev <matt.melo98@gmail.com>
2019-06-03 20:08:09 -0300

    Teste Inoex em Node

---
+++ index.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+const api = axios.create({
+    baseURL: 'https://api.blockcypher.com/v1/bcy/test'
+});
+
+main();
+
+function main () {
+    const address = 'C3qXd3YYRBrZZod5D9GLGhpDNnVfAwpbe2';
+    const amount = '0.1';
+    const token = 'f559581fbebb412b9379cd5752bcae49'
+    api.post(`/faucet?token=?${token}`, {
+        address,
+        amount
+    }).then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log(err.response.data.error);
+    });
+}
+
